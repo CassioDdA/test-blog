946bf5b0d707aaa0c2809303c227d87f8c4a1e39
CássioD <cassioddev@gmail.com>
2024-02-24 22:11:54 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,15 @@
+export const dataFake = [
+ {
+  "id":'1',
+  "title":'ee',
+  "description":'Lorem ipsum dolor sit amet, consectetur adipisicing elit...',
+  "photoCover":'https://th.bing.com/th/id/OIP.CB_YyUZV1UkXAywQhgrDlAHaEi?rs=1&pid=ImgDetMain'
+ },
+ {
+  "id":'2',
+  "title":'My News',
+  "description":'Lorem ipsum dolor sit amet, consectetur adipisicing elit...',
+  "photoCover":''
+ }
+
+]
